002ecc2ff758248a33c871d0ae847cf3e381ad42
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-11-23 01:56:09 -0500

    minor

--- devqon/src-tauri/downquark/particle_flows/src/flows/application_launch/splash.rs
+++ devqon/src-tauri/downquark/particle_flows/src/flows/application_launch/splash.rs
@@ -1,7 +1,7 @@
 /*
 1. show splash screen
 1. config and paths should be stored in state by this point
-1. use them to check for base ~/.dq dir and .etc
+1. use them to check for base ~/.dq dir, etc.
 1. create and /or read template file
 1. mock(for now) version check
 1. mock(for now) user is logged in -- or maybe this can be read as an opt from the conf?
